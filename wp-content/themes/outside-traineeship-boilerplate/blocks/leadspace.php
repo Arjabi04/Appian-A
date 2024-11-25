17a828efd71dc4a56e9cf1d464018080721ddc05
Pawan <pawandongol@gmail.com>
2024-11-25 10:15:55 +0545

    leadspace image update

--- wp-content/themes/outside-traineeship-boilerplate/blocks/leadspace.php
+++ wp-content/themes/outside-traineeship-boilerplate/blocks/leadspace.php
@@ -1,7 +1,7 @@
 <?php
 if ( is_admin() ) :
     /* Render screenshot for example */
-    $imgUrl = get_stylesheet_directory_uri() . '/assets/images/leadspace.png';
+    $imgUrl = get_stylesheet_directory_uri() . '/assets/images/leadspace.webp';
     echo '<img src="' . $imgUrl . '">';
 else:
     $content = get_field('leadspace');
