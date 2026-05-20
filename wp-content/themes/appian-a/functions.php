a0a8efe3d1557d0bdc9b07d1de5fbd5ec7a548f1
SanskritiAcharya <sanskriti.acharyaaa27@gmail.com>
2026-05-20 16:47:35 +0545

    testing

--- wp-content/themes/appian-a/functions.php
+++ wp-content/themes/appian-a/functions.php
@@ -11,7 +11,6 @@ if ( ! defined( '_S_VERSION' ) ) {
 	// Replace the version number of the theme on each release.
 	define( '_S_VERSION', '1.0.0' );
 }
-
 function vite_assets($entry) {
     static $manifest;
     if (!$manifest) {
