755f7471b86ae1f47f71a9a9a2830914b1369bab
SanskritiAcharya <sanskriti.acharyaaa27@gmail.com>
2026-06-08 11:45:14 +0545

    Feat: Backend for hero projects, also fixes in our-work module

--- wp-content/themes/appian-a/template-parts/components/project-card.php
+++ wp-content/themes/appian-a/template-parts/components/project-card.php
@@ -1,8 +1,23 @@
+<?php
+$card_image  = $card_data['image']       ?? null;
+$category    = $card_data['category']    ?? '';
+$title       = $card_data['title']       ?? '';
+$description = $card_data['description'] ?? '';
+$link        = $card_data['link']        ?? null;
+$link_url    = $link['url']              ?? '#';
+$link_target = $link['target']           ?? '_self';
+
+if (empty($title)) {
+    return;
+}
+?>
 <article class="project-card">
-    <img
-        class="project-card__image"
-        src="/wp-content/themes/appian-a/resources/images/card-bg.png"
-        alt="Aerial view of a modern building">
+    <?php if (!empty($card_image) && !empty($card_image['url'])) : ?>
+        <img
+            class="project-card__image"
+            src="<?php echo esc_url($card_image['url']); ?>"
+            alt="<?php echo esc_attr($card_image['alt'] ?? ''); ?>">
+    <?php endif; ?>
 
     <div class="project-card__meta d-flex align-items-start">
         <img
@@ -11,18 +26,29 @@
             alt=""
             aria-hidden="true">
 
-        <span class="project-card__category body-xsmall">Renovation &amp; Remodeling | Waterproofing &amp; Solutions</span>
+        <?php if (!empty($category)) : ?>
+            <span class="project-card__category body-xsmall">
+                <?php
+                $category_parts = preg_split('/\s+/', trim($category));
+                $first_word = !empty($category_parts[0]) ? $category_parts[0] : '';
+                ?>
+                <span class="d-inline d-xl-none"><?php echo esc_html($category); ?></span>
+                <span class="d-none d-xl-inline"><?php echo esc_html($first_word); ?></span>
+            </span>
+        <?php endif; ?>
     </div>
 
     <div class="project-card__content">
-        <h3 class="project-card__title h6">Modern Building Solutions</h3>
-        <p class="project-card__description body-xsmall">Smart designs built efficiently</p>
+        <h3 class="project-card__title h6"><?php echo esc_html($title); ?></h3>
+        <?php if (!empty($description)) : ?>
+            <p class="project-card__description body-xsmall"><?php echo nl2br(esc_html($description)); ?></p>
+        <?php endif; ?>
     </div>
 
-    <div class="project-card__read-more">
+    <a href="<?php echo esc_url($link_url); ?>" class="project-card__read-more"<?php echo $link_target === '_blank' ? ' target="_blank" rel="noopener noreferrer"' : ''; ?>>
         <span class="project-card__arrow" aria-hidden="true">
             <img src="/wp-content/themes/appian-a/resources/images/svgs/arrow-right.svg" alt="">
         </span>
-        <span class="project-card__read-more-text">read more</span>
-    </div>
+        <span class="project-card__read-more-text"><?php echo esc_html(!empty($link['title']) ? $link['title'] : 'read more'); ?></span>
+    </a>
 </article>
